e89db351bcbeeb53e7992a7cc13d1499d6e7b042
Alex Jensen <96dalex@gmail.com>
2016-01-15 15:27:46 -0700

    implement cache based on hash of mix.exs for bash

--- mix_autocomplete.sh
+++ mix_autocomplete.sh
@@ -1,10 +1,28 @@
-__setup_mix_autocomplete() {
-  if ! type mix &> /dev/null; then
+_mix() {
+  if ! type mix &> /dev/null || ! [ -e mix.exs ]; then
     return
   fi
 
-  if [ -e mix.exs ]; then
-    complete -W "$(
+  local current_mix_md5
+
+  if type "md5" &>/dev/null; then
+    current_mix_md5=`md5 -q mix.exs`
+  elif type "md5sum" &>/dev/null; then
+    current_mix_md5=`md5sum mix.exs | awk '{ print $1 }'`
+  else
+    # no either md5 or md5sum
+    return 1
+  fi
+
+  if [[ $current_mix_md5 != $_last_mix_md5 ]]; then
+    mix compile >/dev/null 2>&1
+    if [[ $? -eq 1 ]]; then
+      # Project compilation failed --
+      # show a message explaining why we can't show completions?
+      return 1
+    fi
+    _last_mix_md5=$current_mix_md5
+    _mix_task_list="$(
       mix run -e '
         Mix.Task.load_all
           |> Enum.map(&(Mix.Task.task_name &1))
@@ -12,14 +30,11 @@ __setup_mix_autocomplete() {
           |> Enum.join(" ")
           |> IO.puts
       '
-    )" mix
-  else
-    complete -W "" mix
+    )"
   fi
-}
-cd() {
-  builtin cd "$@" || return
-  __setup_mix_autocomplete
-}
 
-__setup_mix_autocomplete
+  cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$_mix_task_list" $cur) )
+  return 0
+}
+complete -F _mix mix
